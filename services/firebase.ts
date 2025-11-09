daf0348c85a43c7c70185c8debbc51a6475c0f90
gurkankose1 <gurkankose1@gmail.com>
2025-11-09 16:14:15 +0300

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,21 +1,22 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// DİKKAT: Bu bilgileri Firebase projenizin ayarlarından alıp buraya yapıştırmanız gerekmektedir.
-// Project Settings > General > Your apps > Web app > SDK setup and configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAaVJLw4VXl3wR9Tt2SzsJwciJICH6j_uI",
+  authDomain: "skynewstr.firebaseapp.com",
+  projectId: "skynewstr",
+  storageBucket: "skynewstr.firebasestorage.app",
+  messagingSenderId: "565149586636",
+  appId: "1:565149586636:web:372dec9553aa0a465b5e69",
+  measurementId: "G-Q73SE2T6SN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
